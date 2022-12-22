52d630ef95b3e454244abb961313972f6893c667
代码厨子 <hofeng@aqifun.com>
2022-12-22 14:55:37 +0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# 用于自动上传到阿里云OSS的vite插件
+
+## 安装
+
+通过npm安装
+
+```bash
+npm install vite-ali-oss-plugin -D
+```
+
+## 配置
+
+设置option
+
+## 用法
+
+这里是具体用法
---
+++ src/index.ts
@@ -0,0 +1,126 @@
+import path from "path";
+import glob from "glob";
+import OSS from "ali-oss";
+import { normalizePath } from "vite";
+
+interface Option {
+  region: string;
+  accessKeyId: string;
+  accessKeySecret: string;
+  bucket: string;
+  basePath?: string;
+  enabled?: boolean;
+  overwrite?: boolean;
+  ignore?: string;
+  headers?: any;
+  test?: string | any;
+}
+
+export default function viteAliOssPlugin(options: Option) {
+  let basePath = "/";
+  let buildConfig: any = {};
+
+  if (options.enabled !== void 0 && !options.enabled) {
+    return;
+  }
+
+  return {
+    name: "vite-ali-oss-plugin",
+    enforce: "post",
+    apply: "build",
+    configResolved(config: any) {
+      basePath = config.base;
+      buildConfig = config.build;
+    },
+    async closeBundle() {
+      const outDirPath = normalizePath(path.resolve(normalizePath(buildConfig.outDir)));
+
+      const createOssOption = Object.assign({}, options);
+      delete createOssOption.overwrite;
+      delete createOssOption.ignore;
+      delete createOssOption.headers;
+      delete createOssOption.test;
+      delete createOssOption.enabled;
+
+      const client = new OSS(createOssOption);
+
+      const files = glob.sync(outDirPath + "/**/*", {
+        strict: true,
+        nodir: true,
+        dot: true,
+        ignore: options.ignore ? options.ignore : "",
+      });
+      console.log("");
+      console.log("ali oss upload start");
+      console.log("");
+
+      const startTime = new Date().getTime();
+      if (options.basePath) {
+        basePath = options.basePath;
+      }
+      basePath = (basePath || "").replace(/\/$/, "");
+      console.log(basePath);
+      if (options.overwrite) {
+        const fileList = await client.listV2(
+          {
+            "max-keys": "1000",
+            prefix: basePath ? `${basePath}/` : "",
+          },
+          {}
+        );
+        let objs: string[] = [];
+        fileList.objects.forEach((ele) => {
+          objs.push(ele.name);
+        });
+        if (objs.length) {
+          await client.deleteMulti(objs, { quiet: true });
+          console.log("");
+          console.log(`delete complete!`);
+          console.log("");
+        }
+      }
+
+      console.log(basePath);
+      for (const fileFullPath of files) {
+        const filePath = fileFullPath.split(outDirPath)[1];
+
+        const ossFilePath = basePath + filePath;
+        const completePath = `${options.region}: ${options.bucket}: ${ossFilePath}`;
+
+        const output = `${buildConfig.outDir + filePath} => ${completePath}`;
+        if (options.test) {
+          console.log(`test upload path: ${output}`);
+          continue;
+        }
+
+        if (options.overwrite) {
+          await client.put(ossFilePath, fileFullPath, {
+            headers: options.headers || {},
+          });
+          console.log(`upload complete: ${output}`);
+        } else {
+          try {
+            await client.head(ossFilePath);
+            console.log(`files exists: ${output}`);
+          } catch (err: any) {
+            if (err.code === "NoSuchKey") {
+              await client.put(ossFilePath, fileFullPath, {
+                headers: Object.assign(options.headers || {}, {
+                  "x-oss-forbid-overwrite": true,
+                }),
+              });
+              console.log(`upload complete: ${output}`);
+            } else {
+              throw new Error(err);
+            }
+          }
+        }
+      }
+      const duration = (new Date().getTime() - startTime) / 1000;
+
+      console.log("");
+      console.log(`ali oss upload complete ^_^, cost ${duration.toFixed(2)}s`);
+      console.log("");
+    },
+  };
+}
